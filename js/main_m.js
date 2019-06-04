2bf495caf642cf65a5ffe262a6652ddd59431df2
Marina_Zhan <931628706@qq.com>
2019-06-04 16:56:34 +0800

    选择文件和鼠标悬浮样式

--- js/main_m.js
+++ js/main_m.js
@@ -1,9 +1,89 @@
 window.onload = function(){
+	//--------------------点击事件---------------
+	//#解析
 	$("#yes").click(function(){
 		$("#notChooseRvm").css("display","flex");
-		//console.log("点击我了");
 	});
 	$("#errorYes").click(function(){
 		$("#notChooseRvm").hide();
-	})
+	});
+	//#选择文件
+	$("#chooseFile>.content>div.rvm").on("click",">span",function(e){
+		$("#chooseFile>.content>div.rvm>span").removeClass("active");
+		$(this).addClass("active");
+	});
+	$("#chooseFile>.content>div.att").on("click",">span",function(e){
+		$("#chooseFile>.content>div.att>span").removeClass("active");
+		$(this).addClass("active");
+	});
+	 /* $.ajax({
+		type:"POST",
+		url:"http://www.toolkip.com/haiyouservice/seervmWebService1.asmx?aspxerrorpath=/haiyouservice/seervmWebService1.asmx/allFiles",
+		ansyc:true,
+		data:{},
+		success:function(data){
+			console.log(data);
+		},
+		error:function(e){
+			console.log(e.responseText);
+		}
+		
+	}); */
+	//
+	$(".submissionArea .icon").click(
+		
+	);
+	var fileArray = [{"RVM":"files/rvm/项目1.RVM","ATT":"files/ATT/项目1.ATT"},{"RVM":"files/rvm/项目2.RVM","ATT":"files/ATT/项目2.ATT"},{"RVM":"files/rvm/项目3.RVM","ATT":"files/ATT/项目3.ATT"},{"RVM":"files/rvm/项目4.RVM","ATT":"files/ATT/项目4.ATT"},{"RVM":"files/rvm/项目5.RVM","ATT":"files/ATT/项目5.ATT"}];
+	//fileArray = [];
+	arrayMnplton(fileArray);
+	
 }
+
+
+function arrayMnplton(fileArray){
+	var attFiles = [];
+	var rvmFiles = [];
+	for(let i=0;i<fileArray.length;i++){
+		if(fileArray[i].ATT){
+			attFiles.push(fileArray[i].ATT);
+			
+		}
+		if(fileArray[i].RVM){
+			rvmFiles.push(fileArray[i].RVM);
+		}
+	}
+	nofileOrNot(0,rvmFiles);
+	nofileOrNot(1,attFiles);
+	
+	
+}
+
+function nofileOrNot(sig,files){
+	switch(sig){
+		//rvm
+		case 0:
+			if(files.length!=0){
+				$("#chooseFile>.content>.rvm>.noFile").hide();
+			}
+			showFileName("rvm",files);
+			break;
+		//att
+		case 1:
+			if(files.length!=0){
+				$("#chooseFile>.content>.att>.noFile").hide();
+				showFileName("att",files);
+			}
+			break;
+			
+	}
+}
+
+function showFileName(id,files){
+	for(let i=0;i<files.length;i++){
+		let currentSpan = "<span>" + files[i] + "</span>";
+		let parentNode = "#chooseFile>.content>.";
+		$(parentNode+id).append(currentSpan); 
+	}
+}
+
+
