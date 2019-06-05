5f6e44fe9668d7937f38c8c2af9007670defd78f
Marina_Zhan <931628706@qq.com>
2019-06-05 16:33:03 +0800

    页面完善

--- js/main_m.js
+++ js/main_m.js
@@ -1,81 +1,165 @@
 window.onload = function(){
 	
 	//--------------------点击事件---------------
-	//#打开
+	//#文件>打开
 	$("#nav>.menu-area .open").click(function(){
 		$("#chooseFile").show();
+		//获取服务器文件
+		$.ajax({
+			type:"POST",
+			url:"http://www.toolkip.com/haiyouservice/seervmWebService1.asmx/allFiles",
+			ansyc:true,
+			data:{},
+			success:function(data){
+				console.log(data);
+				var allFiles = eval(data);
+				console.log(allFiles);
+				arrayMnplton(allFiles);
+				/* allFiles = null; */
+			},
+			error:function(e){
+				console.log(e.responseText);
+			}
+		});
+	});
+	//#删除 ,#取消 
+	$("#chooseFile>.title>.delete,.submissionArea>.buttonPane>#no").click(function(){
+		//清空强制转换复选框
+		if($("#checkboxIcon").hasClass("select")){
+			$("#checkboxIcon").removeClass("select");
+			$("#checkboxIcon").addClass("unselect");
+		}
+		//清空文件
+		$("#chooseFile>.content .fileList").empty();
+		//隐藏页面
+		$("#chooseFile").hide();
+	});
+	//#取消
+	/* $(".submissionArea>.buttonPane>#no").click(function(){
+		$("#chooseFile").hide();
+	}) */
+	
+	$("#checkboxText").click(function(){
+		$("#chooseFile>.submissionArea .icon").click();
 	});
 	//#复选框
-	 /* $("#chooseFile>.submissionArea .icon").click(function(){ 
-		 console.log("dianji"); */
-		/* $("#chooseFile>.submissionArea .icon").toggle(function(){
-			console.log("单数");
-			$("#chooseFile>.submissionArea .icon").css({
-				"background":"url(./img/openFile/btn_checkbox_selected.png)",
-				"display":"inline-block"
-			});
-		},function(){
-			console.log("偶数");
-			$("#chooseFile>.submissionArea .icon").css({
-				"background":"url(./img/openFile/btn_checkbox_unselected.png)",
-				"display":"inline-block"
-			});
-		},function(){
-			console.log("单数");
-			$("#chooseFile>.submissionArea .icon").css({
-				"background":"url(./img/openFile/btn_checkbox_selected.png)",
-				"display":"inline-block"
-			});
-		}); */
-	/* }); */
+	$("#chooseFile>.submissionArea .icon").click(function(){
+		if($(this).hasClass("unselect")){
+			$(this).removeClass("unselect");
+			$(this).addClass("select");
+		}
+		else if($(this).hasClass("select")){
+			$(this).removeClass("select");
+			$(this).addClass("unselect");
+		}
+		else{
+			//
+		}
+	}); 
 	
-	//#解析
+	//#提交解析
 	$("#yes").click(function(){
-		$("#notChooseRvm").css("display","flex");
+		//未选择rvm
+		if(!$("#chooseFile>.content>.rvm>.fileList>span").hasClass("active")){
+			$("#notChooseRvm").css("display","flex");
+		}
+		else{
+			//解析文件路径
+			var rvmPath = $("#chooseFile>.content>.rvm>.fileList>span.active").text();
+			var attPath = "";
+			//强制转换flag
+			var transformationFlag = 0;
+			
+			if($("#chooseFile>.content>.att>.fileList>span").hasClass("active")){
+				attPath = $("#chooseFile>.content>.att>.fileList>span.active").text();
+			}
+			
+			//是否强制转换
+			if($("#chooseFile>.submissionArea .icon").hasClass("unselect")){
+				transformationFlag = 0;
+			}
+			else{
+				transformationFlag = 1;
+			}
+			
+			console.log(rvmPath);
+			console.log(attPath);
+			console.log(transformationFlag);
+			
+			//提交文件给后台
+			$.ajax({
+				type:"POST",
+				url:"http://www.toolkip.com/haiyouservice/seervmWebService1.asmx/getfiles",
+				ansyc:true,
+				data:{
+					rvm:rvmPath,
+					att:attPath,
+					flag:transformationFlag
+				},
+				success:function(data){
+					console.log(data);
+					/* var allFiles = eval(data);
+					console.log(allFiles);
+					arrayMnplton(allFiles); */
+					/* allFiles = null; */
+				},
+				error:function(e){
+					console.log(e.responseText);
+				}
+				
+			})
+			
+		}
 	});
+	
 	$("#errorYes").click(function(){
 		$("#notChooseRvm").hide();
 	});
 	//#选择文件
-	$("#chooseFile>.content>div.rvm").on("click",">span",function(e){
-		$("#chooseFile>.content>div.rvm>span").removeClass("active");
+	$("#chooseFile>.content>div.rvm>.fileList").on("click",">span",function(e){
+		$("#chooseFile>.content>div.rvm>.fileList>span").removeClass("active");
 		$(this).addClass("active");
-	});
-	$("#chooseFile>.content>div.att").on("click",">span",function(e){
-		$("#chooseFile>.content>div.att>span").removeClass("active");
-		$(this).addClass("active");
-	});
-	 $.ajax({
-		type:"POST",
-		url:"http://www.toolkip.com/haiyouservice/seervmWebService1.asmx/allFiles",
-		ansyc:true,
-		data:{},
-		success:function(data){
-			console.log(data);
-			var sth = eval(data);
-			console.log(sth);
-			//arrayMnplton(eval(data));
-		},
-		error:function(e){
-			console.log(e.responseText);
+		//console.log();
+		if($("#yes").attr("disabled") == "disabled"){
+			$("#yes").attr("disabled",false);
+			$("#yes").css({
+				"background-color":"#347cb6",
+				"cursor":"pointer"
+			});
+			
 		}
-		
+		//if($("#yes").attr("disabled") = "disabled")
 	});
-	//
-	$(".submissionArea .icon").click(
-		
-	);
-	var fileArray = [{"RVM":"files/rvm/项目1.RVM","ATT":"files/ATT/项目1.ATT"},{"RVM":"files/rvm/项目2.RVM","ATT":"files/ATT/项目2.ATT"},{"RVM":"files/rvm/项目3.RVM","ATT":"files/ATT/项目3.ATT"},{"RVM":"files/rvm/项目4.RVM","ATT":"files/ATT/项目4.ATT"},{"RVM":"files/rvm/项目5.RVM","ATT":"files/ATT/项目5.ATT"}];
-	//fileArray = [];
-	arrayMnplton(fileArray);
+	$("#chooseFile>.content>div.att>.fileList").on("click",">span",function(e){
+		$("#chooseFile>.content>div.att>.fileList>span").removeClass("active");
+		$(this).addClass("active");
+		if($("#yes").attr("disabled") == "disabled"){
+			$("#yes").attr("disabled",false);
+			$("#yes").css({
+				"background-color":"#347cb6",
+				"cursor":"pointer"
+			});
+		}
+	});
 	
+	//#上传文件
+	//##rvm
+	$("#chooseFile>.content>.rvm .uploadFile").click(function(){
+		console.log("click");
+		$("#rvmInputFile").click();
+	});
+	//##att
+	$("#chooseFile>.content>.att .uploadFile").click(function(){
+		console.log("click");
+		$("#attInputFile").click();
+	});
 }
 
 
 function arrayMnplton(fileArray){
 	var attFiles = [];
 	var rvmFiles = [];
-	for(let i=0;i<fileArray.length;i++){
+	for(var i=0;i<fileArray.length;i++){
 		if(fileArray[i].ATT){
 			attFiles.push(fileArray[i].ATT);
 			
@@ -86,8 +170,6 @@ function arrayMnplton(fileArray){
 	}
 	nofileOrNot(0,rvmFiles);
 	nofileOrNot(1,attFiles);
-	
-	
 }
 
 function nofileOrNot(sig,files){
@@ -96,14 +178,20 @@ function nofileOrNot(sig,files){
 		case 0:
 			if(files.length!=0){
 				$("#chooseFile>.content>.rvm>.noFile").hide();
+				showFileName("rvm>.fileList",files);
 			}
-			showFileName("rvm",files);
+			else{
+				$("#chooseFile>.content>.rvm>.fileList").hide();
+			}
 			break;
 		//att
 		case 1:
 			if(files.length!=0){
 				$("#chooseFile>.content>.att>.noFile").hide();
-				showFileName("att",files);
+				showFileName("att>.fileList",files);
+			}
+			else{
+				$("#chooseFile>.content>.rvm>.fileList").hide();
 			}
 			break;
 			
@@ -111,11 +199,13 @@ function nofileOrNot(sig,files){
 }
 
 function showFileName(id,files){
-	for(let i=0;i<files.length;i++){
-		let currentSpan = "<span>" + files[i] + "</span>";
-		let parentNode = "#chooseFile>.content>.";
+	var parentNode = "#chooseFile>.content>.";
+	/*$(parentNode+id).empty(); */
+	for(var i=0;i<files.length;i++){
+		var currentSpan = "<span>" + files[i] + "</span>";
 		$(parentNode+id).append(currentSpan); 
 	}
 }
+
 
 
